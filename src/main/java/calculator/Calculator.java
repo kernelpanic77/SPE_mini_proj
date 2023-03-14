d8115dd46d062d2ffeda74815092b28f5a43f49f
Ishan Shanware <shanware.ishan@gmail.com>
2023-03-14 15:16:03 +0530

    Revert back to 743466a

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,12 +2,12 @@ package calculator;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+// import org.apache.logging.log4j.LogManager;
+// import org.apache.logging.log4j.Logger;
 
 public class Calculator {
 
-    private static final Logger logger = LogManager.getLogger(Calculator.class);
+    // private static final Logger logger = LogManager.getLogger(Calculator.class);
     public Calculator() {
     }
 
@@ -73,31 +73,31 @@ public class Calculator {
 
 
     public double factorial(double number1) {
-        logger.info("[FACTORIAL] - " + number1);
+        //logger.info("[FACTORIAL] - " + number1);
         double result = fact(number1);
-        logger.info("[RESULT - FACTORIAL] - " + result);
+        //logger.info("[RESULT - FACTORIAL] - " + result);
         return result;
     }
 
 
 
     public double squareRoot(double number1) {
-        logger.info("[SQ ROOT] - " + number1);
+        //logger.info("[SQ ROOT] - " + number1);
         double result = Math.sqrt(number1);
-        logger.info("[RESULT - SQ ROOT] - " + result);
+        //logger.info("[RESULT - SQ ROOT] - " + result);
         return result;
     }
 
 
     public double power(double number1, double number2) {
-        logger.info("[POWER - " + number1 + " RAISED TO] " + number2);
+        //logger.info("[POWER - " + number1 + " RAISED TO] " + number2);
         double result = Math.pow(number1,number2);
-        logger.info("[RESULT - POWER] - " + result);
+        //logger.info("[RESULT - POWER] - " + result);
         return result;
     }
 
     public double naturalLog(double number1) {
-        logger.info("[NATURAL LOG] - " + number1);
+        //logger.info("[NATURAL LOG] - " + number1);
         double result = 0;
         try {
 
@@ -111,7 +111,7 @@ public class Calculator {
         } catch (ArithmeticException error) {
             System.out.println("[EXCEPTION - LOG] - Cannot find log of negative numbers " + error.getLocalizedMessage());
         }
-        logger.info("[RESULT - NATURAL LOG] - " + result);
+        //logger.info("[RESULT - NATURAL LOG] - " + result);
         return result;
     }
     public double fact(double num) {
